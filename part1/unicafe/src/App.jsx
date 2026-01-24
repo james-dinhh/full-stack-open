5d3f87d034549abeb379344f92f57922af81d001
james-dinhh <jamesdinh.bib@gmail.com>
2026-01-24 16:41:04 +0700

    final ex 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,12 +20,14 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <table>
+      <tbody>
       <StatisticsLine text="good" value={good} />
       <StatisticsLine text="neutral" value={neutral} />
       <StatisticsLine text="bad" value={bad} />
       <StatisticsLine text="all" value={all} />
       <StatisticsLine text="average" value={average} />
       <StatisticsLine text="positive" value={positive + " %"} />
+      </tbody>
     </table>
   );
 };
